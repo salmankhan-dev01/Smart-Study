0550779312b75605c86f0096ae2fb04f31d10c07
Mosalman <jickhirisalmankhan22@gmail.com>
2025-06-03 16:57:58 +0530

    This is first commit to push project

---
+++ app/src/main/java/com/example/smartstudy/presentation/dashboard/DashboardScreen.kt
@@ -0,0 +1,83 @@
+package com.example.smartstudy.presentation.dashboard
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.smartstudy.presentation.component.CountCard
+
+@Composable
+fun DashboardScreen() {
+    Scaffold(
+        topBar = { DashboardScreenTopBar() }
+    ) { paddingValues ->
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
+            item {
+                CountCardsSection(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(10.dp),
+                    subjectCount = 15, studiedHours = "23", goalHours = "54"
+
+                )
+            }
+
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun DashboardScreenTopBar() {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = "StudySmart",
+                style = MaterialTheme.typography.headlineMedium
+            )
+        }
+    )
+}
+
+@Composable
+private fun CountCardsSection(
+    modifier: Modifier,
+    subjectCount: Int,
+    studiedHours: String,
+    goalHours: String,
+) {
+    Row(modifier = modifier) {
+        CountCard(
+            modifier = Modifier.weight(1f),
+            headingText = "Subject Count",
+            count = "$subjectCount"
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        CountCard(
+            modifier = Modifier.weight(1f),
+            headingText = "Studied Hours",
+            count = studiedHours
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        CountCard(
+            modifier = Modifier.weight(1f),
+            headingText = "Goal Study Hours",
+            count = goalHours
+        )
+    }
+}
